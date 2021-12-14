ffc2f67f2b9afb950527eb543d6fca52bbad8dea
Jihun Lee <wlgns3398@naver.com>
2021-12-14 22:47:47 +0900

    구조체 복사 추가

--- struct02_1.c
+++ struct02_1.c
@@ -13,14 +13,12 @@ struct Pointertest{
 };
 
 int main(){
-    struct TEST t1 = {8,"none","none2"};
-    struct TEST *pt;
+    struct TEST t1 = {8,"none","none2"}; //구조체 초기화 값
+    struct TEST *pt; // 구조체 포인터
     pt = &t1;
     printf("%d %s %s\n", pt->a,pt->b,pt->c);
     pt->a=10;
     pt->b="string";
-    printf("%d %s %s\n", pt->a,pt->b,t1.c);
-    
     strcpy(t1.c,"복사한 값");
     printf("%d %s %s\n", pt->a,pt->b,t1.c);
     
@@ -30,18 +28,19 @@ int main(){
     pp= &p1;
     (*pp).num = 1;
     p1.numpointer = &i;
-    printf("%d %d\n", p1.num, *(p1.numpointer));
-  
+    printf("포인터 다루기 %d %d\n", p1.num, *(p1.numpointer));
     /*
   -> 가 * 보다 우선순위가 높으므로 먼저 해석하게 된다.
   *(p1.numpointer) => *((*pp).numpointer) => *pp.numpointer
    */
     *pp->numpointer = 2;
-
-    printf("%d %d\n", p1.num, *((*pp).numpointer));
+    printf("포인터 다루기 %d %d\n", p1.num, *((*pp).numpointer));
     
-
-
-
+    
+    // 구조체 복사
+    struct TEST t2;
+    t2 = t1;
+    printf("구조체 복사: %d %s %s\n", t2.a,t2.b,t1.c);
+    
     return 0;
 }
